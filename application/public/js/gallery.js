6416f37b208d16bdd964ded50e27a87819b6602e
andrei <git1@email.com>
2021-08-02 07:56:47 +0200

    Restructure
    Project restructuring

--- application/public/js/gallery.js
+++
@@ -1,9 +0,0 @@
-var path = sessionStorage.getItem('path');
-if (path != "") {
-  path = path.substring(7);
-  let image = document.createElement('img');
-  image.src = path;
-  image.id = 'lala';
-  let prevImg = document.getElementById("sechelt");
-  prevImg.parentNode.insertBefore(image, prevImg);
-}
